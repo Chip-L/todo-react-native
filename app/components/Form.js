249491df40c421b75b3ab94ff5f65b1238eee37a
clong desktop <chip_long@yahoo.com>
2021-11-10 20:12:23 -0700

    added label and input field

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,24 +1,48 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 function Form(props) {
+  const [task, setTask] = useState("See ME!");
+
+  console.log(props);
   return (
-    <View style={styles.form}>
+    <View style={[styles.form, props.showLayout ? showBorders : {}]}>
       <Text style={[styles.h2, props.showLayout ? showBorders : {}]}>
         What needs to be done?
       </Text>
+      <TextInput
+        style={styles.input}
+        autoComplete="off"
+        onChangeText={setTask}
+        value={task}
+      />
     </View>
   );
 }
 
+const showBorders = {
+  borderColor: "red",
+  borderStyle: "solid",
+  borderWidth: 1,
+};
+
 const styles = StyleSheet.create({
-  form: {},
+  form: {
+    alignContent: "center",
+    marginVertical: 10,
+  },
   h2: {
     color: "rgb(77, 77, 77)",
     fontSize: 24,
     fontWeight: "300",
     textAlign: "center",
-    marginBottom: 10,
+    // marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
--- app/screens/ToDoScreen.js
+++ app/screens/ToDoScreen.js
@@ -5,22 +5,43 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  View,
 } from "react-native";
+import Form from "../components/Form";
 
 function ToDoScreen(props) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>ToDoMatic</Text>
+      <Text style={[styles.title, props.showLayout ? showBorders : {}]}>
+        ToDoMatic
+      </Text>
+      <Form showLayout={props.showLayout} />
     </SafeAreaView>
   );
 }
 
+const showBorders = {
+  borderColor: "red",
+  borderStyle: "solid",
+  borderWidth: 1,
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingHorizontal: 10,
+    backgroundColor: "#ddd",
   },
-  title: {},
+  title: {
+    color: "rgb(77, 77, 77)",
+    fontSize: 38,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 10,
+  },
 });
 
 export default ToDoScreen;
